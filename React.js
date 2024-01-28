b47e8df91f5f8eef7da3c73ed42b3c62379c7594
sahil1717kumar <sahil1717kumar@gmail.com>
2024-01-28 17:22:14 +0530

    React without JSX

---
+++ React.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "header" }, [
+    React.createElement("h1", {}, "First Heading"),
+    React.createElement("h2", {}, "First Sub-Heading"),
+  ]),
+  React.createElement("div", { id: "footer" },[
+    React.createElement("h1", {}, "Second Heading"),
+    React.createElement("h2", {}, "Second Sub-Heading"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
